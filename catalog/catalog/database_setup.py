e571e2a84fcff44cc04782747531b6794ce3e6ca
Patrick Martin <pcmart03@gmail.com>
2015-12-05 15:34:24 -0500

    making primary key for photo unique

--- catalog/catalog/database_setup.py
+++ catalog/catalog/database_setup.py
@@ -29,7 +29,7 @@ class Category(Base):
 
 class Photo(Base):
     __tablename__ = "photo"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True)
     filename = Column(String(250), nullable=False)
     alt_text = Column(String(120), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
