5eec5d267a2ac2dbd4c9e8a91eeadb26e91e8779
Patrick Martin <pcmart03@gmail.com>
2015-12-05 19:33:43 -0500

    updated facebook login

--- catalog/catalog/__init__.py
+++ catalog/catalog/__init__.py
@@ -142,9 +142,9 @@ def fbconnect():
     print "access token received %s" % access_token
 
     app_id = json.loads(
-        open('fb-client-secrets.json', 'r').read())['web']['app_id']
+        open('/var/www/FSND-P3-Catalog-App/catalog/catalog/fb-client-secrets.json', 'r').read())['web']['app_id']
     app_secret = json.loads(
-        open('fb-client-secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/FSND-P3-Catalog-App/catalog/catalog/fb-client-secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
